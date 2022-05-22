70e559095d8aa53c84fa3b0aaf34c7daf1a42983
Zachary Cecchetti <zcecchetti@gmail.com>
2022-05-21 22:35:17 -0700

    Buttons in header to change language between english and spanish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './stylesheet.css';
 
-console.log('hi');
+// change language to English
+const englishButton = document.getElementById('english');
+
+englishButton.addEventListener('click', () => {
+  const esMatches = document.querySelectorAll('.spanish');
+  esMatches.forEach((match) => {
+    match.classList.remove('visible');
+  });
+  const enMatches = document.querySelectorAll('.english');
+  enMatches.forEach((match) => {
+    match.classList.add('visible');
+  });
+});
+
+// change language to Spanish
+const spanishButton = document.getElementById('spanish');
+
+spanishButton.addEventListener('click', () => {
+  const enMatches = document.querySelectorAll('.english');
+  enMatches.forEach((match) => {
+    match.classList.remove('visible');
+  });
+  const esMatches = document.querySelectorAll('.spanish');
+  esMatches.forEach((match) => {
+    match.classList.add('visible');
+  });
+});
