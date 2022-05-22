96fda3581be0b3ef950a9350d5a2ac4bff0c5bc5
Zachary Cecchetti <zcecchetti@gmail.com>
2022-05-22 01:08:25 -0700

    Page reloads to top when language is changed by user

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import './stylesheet.css';
 
+// set preference to selected language
+function setPreference(langPreference) {
+  localStorage.setItem('language', JSON.stringify(langPreference));
+}
+
 const englishButton = document.getElementById('english');
 const spanishButton = document.getElementById('spanish');
 
-// change language to English
-englishButton.addEventListener('click', () => {
+// create functions to change site language
+function changeToEnglish() {
   // remove visible class from Spanish translations
   const esMatches = document.querySelectorAll('.spanish');
   esMatches.forEach((match) => {
@@ -19,10 +24,9 @@ englishButton.addEventListener('click', () => {
   // make spanishButton visible and hide englishButton
   spanishButton.classList.remove('hidden');
   englishButton.classList.add('hidden');
-});
+}
 
-// change language to Spanish
-spanishButton.addEventListener('click', () => {
+function changeToSpanish() {
   // remove visible class from English translations
   const enMatches = document.querySelectorAll('.english');
   enMatches.forEach((match) => {
@@ -36,4 +40,34 @@ spanishButton.addEventListener('click', () => {
   // make englishButton visible and hide spanishButton
   englishButton.classList.remove('hidden');
   spanishButton.classList.add('hidden');
+}
+
+// change language to English
+englishButton.addEventListener('click', () => {
+  // call changeToEnglish
+  changeToEnglish();
+
+  // set user language preference to English
+  const langPreference = 'english';
+  setPreference(langPreference);
 });
+
+// change language to Spanish
+spanishButton.addEventListener('click', () => {
+  // call changeToSpanish
+  changeToSpanish();
+
+  // set user language preference to Spanish
+  const langPreference = 'spanish';
+  setPreference(langPreference);
+});
+
+// create/check language preference for user and store in localstorage
+if (localStorage.getItem('language')) {
+  const langPreference = JSON.parse(localStorage.getItem('language'));
+  if (langPreference === 'spanish') {
+    changeToSpanish();
+  } else {
+    changeToEnglish();
+  }
+}
