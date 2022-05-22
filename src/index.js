2d35266211b5bf4e9860a442a89003c628a63228
Zachary Cecchetti <zcecchetti@gmail.com>
2022-05-22 00:07:47 -0700

    Added site logo and moved language list to footer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import './stylesheet.css';
 
-// change language to English
 const englishButton = document.getElementById('english');
+const spanishButton = document.getElementById('spanish');
 
+// change language to English
 englishButton.addEventListener('click', () => {
+  // remove visible class from Spanish translations
   const esMatches = document.querySelectorAll('.spanish');
   esMatches.forEach((match) => {
     match.classList.remove('visible');
   });
+
   const enMatches = document.querySelectorAll('.english');
   enMatches.forEach((match) => {
     match.classList.add('visible');
   });
+
+  // make spanishButton visible and hide englishButton
+  spanishButton.classList.remove('hidden');
+  englishButton.classList.add('hidden');
 });
 
 // change language to Spanish
-const spanishButton = document.getElementById('spanish');
-
 spanishButton.addEventListener('click', () => {
+  // remove visible class from English translations
   const enMatches = document.querySelectorAll('.english');
   enMatches.forEach((match) => {
     match.classList.remove('visible');
@@ -26,4 +32,8 @@ spanishButton.addEventListener('click', () => {
   esMatches.forEach((match) => {
     match.classList.add('visible');
   });
+
+  // make englishButton visible and hide spanishButton
+  englishButton.classList.remove('hidden');
+  spanishButton.classList.add('hidden');
 });
