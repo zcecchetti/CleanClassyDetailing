53fffab204bd074c8ea513aadb99b1f12d5344ce
Zachary Cecchetti <zcecchetti@gmail.com>
2022-06-05 23:46:28 -0700

    Finished first comoplete draft of computer viewport styling

--- src/index.js
+++ src/index.js
@@ -9,37 +9,37 @@ const englishButton = document.getElementById('english');
 const spanishButton = document.getElementById('spanish');
 
 // create functions to change site language
-function changeToEnglish() {
+function changeToSpanish() {
   // remove visible class from Spanish translations
   const esMatches = document.querySelectorAll('.spanish');
   esMatches.forEach((match) => {
-    match.classList.remove('visible');
+    match.classList.remove('hidden');
   });
 
   const enMatches = document.querySelectorAll('.english');
   enMatches.forEach((match) => {
-    match.classList.add('visible');
+    match.classList.add('hidden');
   });
 
   // make spanishButton visible and hide englishButton
-  spanishButton.classList.remove('hidden');
-  englishButton.classList.add('hidden');
+  spanishButton.classList.add('hidden');
+  englishButton.classList.remove('hidden');
 }
 
-function changeToSpanish() {
+function changeToEnglish() {
   // remove visible class from English translations
   const enMatches = document.querySelectorAll('.english');
   enMatches.forEach((match) => {
-    match.classList.remove('visible');
+    match.classList.remove('hidden');
   });
   const esMatches = document.querySelectorAll('.spanish');
   esMatches.forEach((match) => {
-    match.classList.add('visible');
+    match.classList.add('hidden');
   });
 
   // make englishButton visible and hide spanishButton
-  englishButton.classList.remove('hidden');
-  spanishButton.classList.add('hidden');
+  englishButton.classList.add('hidden');
+  spanishButton.classList.remove('hidden');
 }
 
 // change language to English
@@ -77,5 +77,5 @@ const popupButton = document.getElementById('popupButton');
 const promotion = document.getElementById('promotion');
 
 popupButton.addEventListener('click', () => {
-  promotion.classList.remove('visible');
+  promotion.classList.add('hidden');
 });
